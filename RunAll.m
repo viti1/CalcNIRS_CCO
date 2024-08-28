50890eee688124068344349772001abc31aacab7
Vika Lab PC <viti.tr@gmail.com>
2024-08-28 21:11:53 +0300

    update RunAll

--- RunAll.m
+++ RunAll.m
@@ -4,24 +4,25 @@ recordsFolder = [ mFolder '\..\..\Records\UK_EXPERIMENTS_2024\Laser phantom data
 recordsDir = dir([ recordsFolder 'Laser*']);
 records = strcat(recordsFolder, {recordsDir.name})';
 
-
+%%
+prefix = '\results1\oxCCO_redCCO_';
 figs = nan(size(numel(records)));
 for ri = 1:numel(records)
-    [conc, timeVec,figs(ri)] = CalcNIRS([records{ri} '\results.mat'], [3,1],'adult_head',1:2);
+    [conc, time_vector,figs(ri),substanceNames] = CalcNIRS([records{ri} '\results.mat'], [3,1],'adult_head',1:2);
     [~,recName ]  = fileparts(records{ri});
-    savefig(figs(ri),[mFolder '\figs\oxCCO_' recName '.fig']);
-    saveas(figs(ri),[mFolder '\figs\oxCCO_' recName '.png']);
-    save([ mFolder '\figs\oxCCO_' recName '.mat' ],'conc', 'timeVec');
+    savefig(figs(ri),[mFolder prefix recName '.fig']);
+    saveas(figs(ri),[mFolder prefix recName '.png']);
+    save([ mFolder prefix recName '.mat' ],'conc', 'time_vector','substanceNames');
 end
 
 %% save to PPT
 import mlreportgen.ppt.*
 
 % Define the directory containing your saved figures
-figureDir = [ mFolder '/figs/' ]; % Current directory
+figureDir = [ mFolder fileparts(prefix) '\' ]; % Current directory
 
 % Start PowerPoint
-ppt = Presentation([ figureDir '/NIRSwithYeast_1.pptx' ]);
+ppt = Presentation([ figureDir '/NIRSwithYeast_oxCCO_redCCO.pptx' ]);
 open(ppt);
 
 % List of figure files
